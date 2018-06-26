dd6e003222496fa5b3a4868ed7c1ca8c0e5b955e
duccar <duccarrich@gmail.com>
2018-06-26 11:01:35 +0700

    first commit

---
+++ BAITHICShap/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BAITHICShap.Properties;
+
+namespace BAITHICShap
+{
+    internal class Program
+    {
+        private static List<Product> list = new List<Product>();
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1.Add product records");
+                Console.WriteLine("2.Display product records");
+                Console.WriteLine("3.Delete product by Id");
+                Console.WriteLine("4.Exit");
+                Console.WriteLine("Please input your choice ");
+                int ans = Int32.Parse(Console.ReadLine());
+
+                switch (ans)
+                {
+                    case 1:
+                        Add();
+                        break;
+                    case 2:
+                        Display();
+                        break;
+                    case 3:
+                        delete();
+                        break;
+                    case 4:
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+
+        private static void delete()
+        {
+            Console.WriteLine("Nhap Product ID :");
+            string id = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+               
+                if (list[i].ProductId.Equals(id))
+                {
+                    list.RemoveAt(i);
+                }
+            }
+        }
+
+
+        private static void Display()
+        {
+           
+            
+            Console.WriteLine("Dispaly product");
+            Console.WriteLine("{0, -20}{1, -20}{2, -20}","Product ID","Product Name", "Price");
+            Console.WriteLine("--------------------------------------------------------------");
+            foreach (var i in list)
+            {
+                Console.WriteLine("{0,-20}{1,-20}{2,-20}",i.ProductId, i.ProductName, i.ProductPrice);
+            }
+        }
+
+        private static void Add()
+        {
+            while (true)
+            {
+                Console.WriteLine("nhap ProductID: ");
+                string productID = Console.ReadLine();
+                Console.WriteLine("nhap name of product: ");
+                string productName = Console.ReadLine();
+                Console.WriteLine("nhap price: ");
+                string productPrice = Console.ReadLine();
+                Product product = new Product(productID, productName, productPrice);
+                list.Add(product);
+                Console.WriteLine("ban co muon nhap tiep khong(y/n)");
+                string answers = Console.ReadLine();
+                if (answers.Equals("n"))
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ BAITHICShap/Properties/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+namespace BAITHICShap.Properties
+{
+    public class Product
+    {
+      
+        private string _productID;
+        private string _productName;
+        private string _productPrice;
+
+        public Product()
+        {
+        }
+
+        public Product(string productId, string productName, string productPrice)
+        {
+            _productID = productId;
+            _productName = productName;
+            _productPrice = productPrice;
+        }
+
+        public string ProductId
+        {
+            get { return _productID; }
+            set { _productID = value; }
+        }
+
+        public string ProductName
+        {
+            get { return _productName; }
+            set { _productName = value; }
+        }
+
+        public string ProductPrice
+        {
+            get { return _productPrice; }
+            set { _productPrice = value; }
+        }
+
+        public void Add()
+        {
+            
+        } 
+        public void Display()
+        {
+            
+        }
+        public void Delete()
+        {
+            
+        }
+        
+    }
+}
